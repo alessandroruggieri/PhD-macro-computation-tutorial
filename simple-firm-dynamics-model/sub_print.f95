a3d50d9cd3c10675e33918b45cb342e21478a7bd
Alessandro Ruggieri <73037589+alessandroruggieri@users.noreply.github.com>
2020-11-21 23:34:08 +0100

    fortran codes added

---
+++ simple-firm-dynamics-model/sub_print.f95
@@ -0,0 +1,12 @@
+subroutine sub_print
+use globals
+    implicit none
+
+! Print output on screen
+    print *, '------------------------------'
+    print *, "Entry value:", EV
+    print *, '------------------------------'
+
+
+
+end subroutine

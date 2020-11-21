66d83578ff706013f57cc8601955730f53d238a2
Alessandro Ruggieri <73037589+alessandroruggieri@users.noreply.github.com>
2020-11-21 23:45:43 +0100

    added fortran codes

--- simple-firm-dynamics-model/globals.f95
+++
@@ -1,8 +0,0 @@
-module globals
-! Declare integer variables
-    integer :: gp_a, periods, agents
-! Declare real variables
-    real :: alfa, beta, rho, delta, ch, chc, centry, tolk, tolV, a_min, a_max, EV
-! Declare real arrays
-    real, dimension(1:2) :: z
-end module

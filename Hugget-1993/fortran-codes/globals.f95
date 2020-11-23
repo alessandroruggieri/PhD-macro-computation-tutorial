85a3aa65272f4b7266feaa0cea8c3e1138964c1e
Alessandro Ruggieri <73037589+alessandroruggieri@users.noreply.github.com>
2020-11-23 10:44:49 +0100

    fortran codes deleted

--- Hugget-1993/fortran-codes/globals.f95
+++
@@ -1,8 +0,0 @@
-module globals
-! Declare integer variables
-    integer :: gp_a, periods, agents
-! Declare real variables
-    real :: beta, rho, tolk, tolV, a_min, a_max, kmean, kstd, kneg
-! Declare real arrays
-    real, dimension(1:2) :: z
-end module
--- stochastic-NGM/VFI/fortran-codes/globals.f95
+++
@@ -1,8 +0,0 @@
-module globals
-! Declare integer variables
-    integer :: gp_a, periods, agents
-! Declare real variables
-    real :: beta, rho, alfa, delta, a_min, a_max, kmean, cmean, ymean, invmean, tolk, tolV
-! Declare real arrays
-    real, dimension(1:2) :: z
-end module
